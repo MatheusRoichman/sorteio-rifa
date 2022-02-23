9fa5fa60c6a474cf2bd72af809c1e64294d32c96
MatheusRoichman <roichmankill@gmail.com>
2022-02-23 12:04:57 -0300

    Finished first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [ selectedTeam, setSelectedTeam ] = useState<number | string>('aguardando...');
+
+  const getRandomNumberInRange = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Sorteio de equipes</h1>
+
+      <h4 className="font-medium text-xl">A sua equipe é a</h4>
+      <h2 className="font-bold text-3xl">{selectedTeam}</h2>
+
+      <button onClick={ () => setSelectedTeam(getRandomNumberInRange(1, 8)) }>Sortear</button>
+    </>
   );
 }
 
