a7a7b2eec3139e35b036881ae3c9125a074ecd8f
Matheus Roichman <roichmankill@gmail.com>
2022-04-11 11:56:37 -0300

    Escopo do sistema mudado

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const App: React.FC = () => {
-  const [ selectedTeam, setSelectedTeam ] = useState<number | string>('aguardando...');
+  const [selectedNumber, setSelectedNumber] = useState<number | string>(
+    "aguardando..."
+  );
 
-  const getRandomNumberInRange = (min: number, max: number): number => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  const getRandomNumberInRange = (min: number, max: number): number =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
 
   return (
     <>
-      <h1>Sorteio de equipes</h1>
+      <main>
+        <h1>Sorteio da rifa</h1>
 
-      <h4 className="font-medium text-xl">A sua equipe é a</h4>
-      <h2 className="font-bold text-3xl">{selectedTeam}</h2>
+        <h4>O número sorteado foi</h4>
+        <h2>{selectedNumber}</h2>
+      </main>
 
-      <button onClick={ () => setSelectedTeam(getRandomNumberInRange(1, 8)) }>Sortear</button>
+      <button onClick={() => setSelectedNumber(getRandomNumberInRange(1, 300))}>
+        Sortear
+      </button>
+
+      <footer>
+        <p>Sistema 100% confiável. Código fonte em <a href="https://github.com/MatheusRoichman/sorteio-rifa" target="_blank" rel="noopener noreferrer">https://github.com/MatheusRoichman/sorteio-rifa</a></p>
+        <p>Programador: <a href="https://github.com/MatheusRoichman" target="_blank" rel="noopener noreferrer">Matheus Lima Roichman</a></p>
+      </footer>
     </>
   );
-}
+};
 
 export default App;
